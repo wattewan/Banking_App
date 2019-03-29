de7c101443371a06ce4d9248ec32680fb2f93f44
Macintosh HD <macintoshhd@macintoshs-mbp.wifi.bcit.ca>
2019-03-29 14:40:50 -0700

    Login Create

---
+++ login.js
@@ -0,0 +1,80 @@
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+//Load and initialize MessageBird SDK
+var messagebird = require('messagebird')(' '); //Input message bird key here
+
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'password',
+    database : 'nodelogin'
+});
+
+var app = express();
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+app.post('/auth', function(request, response) {
+    var username = request.body.username;
+    var password = request.body.password;
+    if (username && password) {
+        connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+            if (results.length > 0) {
+                request.session.loggedin = true;
+                request.session.username = username;
+                response.redirect('/home');
+            } else {
+                response.send('Incorrect Username and/or Password!');
+            }
+            response.end();
+        });
+    } else {
+        response.send('Please enter Username and Password!');
+        response.end();
+    }
+});
+
+app.get('/home', function(request, response) {
+    if (request.session.loggedin) {
+        response.send('Welcome back, ' + request.session.username + '!');
+    } else {
+        response.send('Please login to view this page!');
+    }
+    response.end();
+});
+
+app.listen(3000);
+
+
+
+connection.connect(function(err) {
+    if (err) {
+        return console.error('error: ' + err.message);
+    }
+
+    console.log('Connected to the MySQL server.');
+});
+
+
+// connection.end(function(err) {
+//     if (err) {
+//         return console.log('error:' + err.message);
+//     }
+//     console.log('Close the database connection.');
+// });
+
